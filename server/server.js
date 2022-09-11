8fe6e6d377d9f0fbce5ca1b49f4355624b068b53
Dary <dady281100@gmail.com>
2022-09-11 17:37:19 -0500

    Components and pages updated as well package.json

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
+const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
-const express = require('express');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+
+//import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//create new Apollo server and pass in our schema data
 const server = new ApolloServer({
-  typeDefs, 
-  resolvers, 
+  typeDefs,
+  resolvers,
   context: authMiddleware
 })
 
+//integrate our Apollo server with Express application as middleware
 server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
@@ -30,10 +35,18 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+
+
+// app.use(routes); comment this out in the end
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  });
 });
 
 process.on('uncaughtException', function(err) {
